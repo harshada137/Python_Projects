462761fce50544c3ccd6b2acfa8915627c7e271e
Harshada Sharad Patil <harshadap1204@gmail.com>
2026-02-09 15:27:23 +0530

    Update main.py

--- Word Count Tool/main.py
+++ Word Count Tool/main.py
@@ -1,1 +1,26 @@
-print("Hello, World!")
+import os
+
+def word_count(filename):
+    with open(filename, "r") as file:
+        text = file.read()
+
+    words = text.split()
+    characters = len(text)
+    lines = text.count("\n") + 1
+
+    print("Total Words:", len(words))
+    print("Total Characters:", characters)
+    print("Total Lines:", lines)
+
+
+def main():
+    print("=== Word Count Tool ===")
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(base_dir, "sample.txt")
+
+    word_count(filename)
+
+
+if __name__ == "__main__":
+    main()
